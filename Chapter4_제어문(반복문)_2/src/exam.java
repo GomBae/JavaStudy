9ecaf269cd2161bb6553ee0bae6c1c15175df632
user <user@DESKTOP-COJ9858>
2022-10-27 17:22:07 +0900

    11

--- Chapter4_제어문(반복문)_2/src/exam.java
+++ Chapter4_제어문(반복문)_2/src/exam.java
@@ -107,17 +107,17 @@ public class exam {
 		
 		System.out.println();
 		
-		for(int i=1;i<=4;i++) {
-			for(int j=1;j<i;j++) {
-				System.out.print(" ");
-			}
-			for(int k=i;k<=4-i;k++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
+//		for(int i=1;i<=4;i++) {
+//			for(int j=1;j<i;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k=i;k<=4-i;k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
 		
 	}
 
