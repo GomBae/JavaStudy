9df1b499f7726342de40d9cb6de520a6a50226c9
user <user@DESKTOP-COJ9858>
2022-10-27 10:06:54 +0900

    _2

--- Chapter4_제어문(반복문)_2/src/반복문_for.java
+++ Chapter4_제어문(반복문)_2/src/반복문_for.java
@@ -1,8 +1,19 @@
-
+/*
+ * 
+ * 
+ */
 public class 반복문_for {
 
 	public static void main(String[] args) {
-		
+		for(int i=1;i<=10;i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		int i=1;
+		while(i<=10) {
+			System.out.print(i + " ");
+			i++;
+		}
 
 	}
 
