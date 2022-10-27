752c11c92540e7981a837e06fe7a83737c2992e0
user <user@DESKTOP-COJ9858>
2022-10-27 10:15:02 +0900

    for2

---
+++ Chapter4_제어문(반복문)_2/src/반복문_for2.java
@@ -0,0 +1,18 @@
+
+public class 반복문_for2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(char ch='A';ch<='Z';ch++) {
+			System.out.print(ch +" ");
+		}
+		System.out.println();
+		char ch = 'A';
+		while(ch<='Z') {
+			System.out.print(ch + " ");
+			ch++;
+		}
+	}
+
+}
