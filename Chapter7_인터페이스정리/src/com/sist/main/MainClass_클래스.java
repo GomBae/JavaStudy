2a76d109f51b04875b5f6eed5e03369a464ee1d2
GomBae <myg177@naver.com>
2022-11-16 09:18:03 +0900

    2022_11_16_인터페이스2

---
+++ Chapter7_인터페이스정리/src/com/sist/main/MainClass_클래스.java
@@ -0,0 +1,42 @@
+package com.sist.main;
+/*
+ *  1) 클래스
+ *    = 클래스 정의하는 방법
+ *    = 객체 생성방법 (new)
+ *    = 메소드 정의하는 방법
+ *    = 생성자 정의하는 방법
+ *    = this키워드 
+ *  2) 상속
+ *    = 자바 상속의 특징
+ *    = super키워드
+ *    = 메소드 오버라이딩
+ *    = final 키워드
+ *    = Object 클래스
+ *  3) 인터페이스 / 추상클래스
+ *    = 인터페이스 / 추상클래스 차이점
+ *    = 인터페이스 / 추상클래스 정의
+ *    = 인터페이스 / 추상클래스 구현방법
+ *    = 인터페이스 상속
+ *    = 인터페이스 default 메소드
+ *  4) 예외처리
+ *    = 예외처리 종류 (예외 복구, 예외 회피)
+ *    = 자바에서 제공하는 예외 처리 클래스 (계층구조)
+ *    = 실행 순서
+ *    = 사용자정의 예외처리 ==> 호출 방법
+ *    ------------------------------------------------
+ *    
+ *  라이브러리
+ *  --------
+ *    = Object, String, StringBuffer, Math, Wrapper ==> java.lang
+ *      StringTokenizer, Date, Calendar, SimpleDateFormat
+ *      List, Set, Map ==> java.util
+ *      IO
+ */
+public class MainClass_클래스 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
